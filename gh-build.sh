e5956feb5833314dc70c64335ca6e3d3537cc9c7
Steph DGenius <stephanetse@gmail.com>
2022-09-14 04:51:07 +0100

    0.1.0-alpha

--- gh-build.sh
+++ gh-build.sh
@@ -4,14 +4,13 @@ apt-get --yes update
 apt-get --yes install wget equivs curl git
 
 deps=$(sed -e '/^#.*$/d; /^$/d; /^\s*$/d' package/dependencies | paste -sd ,)
-git_commit=$(git rev-parse --short HEAD)
 
 printf >configuration "%s\n" \
 	"Section: misc" \
 	"Priority: optional" \
 	"Homepage: https://kaytime.github.io" \
 	"Package: system-core-minimal" \
-	"Version: $git_commit" \
+	"Version: $PROJECT_VERSION" \
 	"Maintainer: Stephane Tsana <stephanetse@gmail.com>" \
 	"Depends: $deps" \
 	"Architecture: all" \
